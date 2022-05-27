5a8afe8c367b33b623aab75b4b82b44af2b406f5
musafir928 <uganoghli@gmail.com>
2022-05-27 11:10:56 +0200

    add dummy data array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 const App = () => {
-    return <div> hello world</div>;
+    const categories = [
+        {
+            id: 1,
+            title: 'Hats'
+        },
+        {
+            id: 2,
+            title: 'Jackets'
+        },
+        {
+            id: 3,
+            title: 'sneakers'
+        },
+        {
+            id: 4,
+            title: 'Womans'
+        },
+        {
+            id: 1,
+            title: 'Mens'
+        }
+    ];
+    return (
+        <div className='categories-container'>
+            {categories.map(({ title, id }) => (
+                <div className='category-container' key={id}>
+                    <div className='background-image'></div>
+                    <div className='category-body-container'>
+                        <h2>{title}</h2>
+                        <p>Shop Now</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default App;
