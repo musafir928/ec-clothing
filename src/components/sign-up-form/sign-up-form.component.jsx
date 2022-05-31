6193f337ebdb25bb29db9d6be5b0402955ff8d62
musafir928 <uganoghli@gmail.com>
2022-06-01 01:44:52 +0200

    create button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -5,6 +5,9 @@ import {
 } from '../../utils/firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+import './sign-up-form.styles.scss';
 
 const initialFormData = {
     displayName: '',
@@ -48,8 +51,9 @@ const SignUpForm = () => {
     };
 
     return (
-        <div className=''>
-            <h1>Sign up with your email and password</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput
                     type='text'
@@ -88,7 +92,7 @@ const SignUpForm = () => {
                     label='confirmPassword'
                 />
 
-                <button type='submit'>Sign Up</button>
+                <Button type='submit'>Sign Up</Button>
             </form>
         </div>
     );
