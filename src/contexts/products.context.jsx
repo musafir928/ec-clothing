81b47552485233fadf28449a69a53de321f0e089
musafir928 <uganoghli@gmail.com>
2022-06-22 21:10:01 +0200

    start async useeffect

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,17 @@
 import { createContext, useState, useEffect } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 export const ProductsContext = createContext({
     products: []
 });
 
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    useEffect(() => {
+        getCategoriesAndDocuments();
+    });
+
+    const [products, setProducts] = useState([]);
     const value = { products };
     return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>;
 };
