6c90edb10e54bea212973e323654536b8d1759f4
musafir928 <uganoghli@gmail.com>
2022-06-30 22:11:36 +0200

    redux- thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,9 +2,13 @@ import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import { useSelector } from 'react-redux';
-import { selectCategoriesMap } from '../../store/categories/categories.selector';
+import {
+    selectCategoriesIsLoading,
+    selectCategoriesMap
+} from '../../store/categories/categories.selector';
 
 import { CategoryContainer, Title } from './category.styles';
 
@@ -12,6 +16,8 @@ const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
     const [products, setProducts] = useState(categoriesMap[category]);
+    const isLoading = useSelector(selectCategoriesIsLoading);
+
     useEffect(() => {
         setProducts(categoriesMap[category]);
     }, [category, categoriesMap]);
@@ -19,10 +25,16 @@ const Category = () => {
     return (
         <Fragment>
             <Title>{category.toUpperCase()}</Title>
-            <CategoryContainer>
-                {products &&
-                    products.map((product) => <ProductCard key={product.id} product={product} />)}
-            </CategoryContainer>
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <CategoryContainer>
+                    {products &&
+                        products.map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                </CategoryContainer>
+            )}
         </Fragment>
     );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -10,7 +10,7 @@ import { rootReducer } from './root-reducer';
 const persistConfig = {
     key: 'root',
     storage,
-    blackList: ['user']
+    whiteList: ['cart']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
