c0cab8f8ad5c92a3c44d948328881ba85f0b4904
musafir928 <uganoghli@gmail.com>
2022-06-30 21:16:08 +0200

    add redux thunk to categories redux

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -1,15 +1,31 @@
+import { CATEGORIES_ACTION_TYPES } from './categories.types';
+
 const INITIAL_STATE = {
-    categories: []
+    categories: [],
+    isLoading: false,
+    error: null
 };
 
 export const categoriesReducer = (state = INITIAL_STATE, action) => {
     const { type, payload } = action;
 
     switch (type) {
-        case 'SET_CATEGORIES':
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
             return {
                 ...state,
-                categories: payload
+                isLoading: true
+            };
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+            return {
+                ...state,
+                isLoading: false,
+                error: payload
+            };
+        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+            return {
+                ...state,
+                categories: payload,
+                isLoading: false
             };
         default:
             return state;
